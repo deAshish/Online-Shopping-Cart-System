0e56e8c13aa868943106912eaec7c5c3bc9d922b
Ashish Ghimire <ashishghimireg@gmail.com>
2023-04-11 23:00:32 -0500

    Project Setup

---
+++ Server/src/main/java/miu/edu/pm/project/onlineshoppingcartsystem/OnlineShoppingCartSystemApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.pm.project.onlineshoppingcartsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingCartSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShoppingCartSystemApplication.class, args);
+    }
+
+}
---
+++ Server/src/test/java/miu/edu/pm/project/onlineshoppingcartsystem/OnlineShoppingCartSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.pm.project.onlineshoppingcartsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingCartSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
