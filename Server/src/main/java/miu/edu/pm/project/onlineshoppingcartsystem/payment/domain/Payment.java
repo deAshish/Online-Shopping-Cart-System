1b84e52860c32fd32d9d57d00ae68e7666d1257a
Ashish Ghimire <ashishghimireg@gmail.com>
2023-04-19 01:19:01 -0500

    Update Payment.java

--- Server/src/main/java/miu/edu/pm/project/onlineshoppingcartsystem/payment/domain/Payment.java
+++ Server/src/main/java/miu/edu/pm/project/onlineshoppingcartsystem/payment/domain/Payment.java
@@ -1,36 +1,36 @@
-//package miu.edu.pm.project.onlineshoppingcartsystem.payment.domain;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import miu.edu.pm.project.onlineshoppingcartsystem.user.domain.Customer;
-//
-//
-//import java.util.Date;
-//
-//@Entity
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//public class Payment {
-//    @Id
-//    @GeneratedValue
-//    Long id;
-//    Date date;
-//    Double amount;
-//    @OneToOne
-//    @JoinColumn(name = "order_id")
-//    Order order;
-//    @OneToOne
-//    @JoinColumn(name = "user_id")
-//    Customer customer;
-//    @OneToOne
-//    @JoinColumn(name = "payment_type_id")
-//    PaymentType paymentType;
-//
-//    public Payment(Date date, Double amount) {
-//        this.date = date;
-//        this.amount = amount;
-//    }
-//}
+package miu.edu.pm.project.onlineshoppingcartsystem.payment.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import miu.edu.pm.project.onlineshoppingcartsystem.user.domain.Customer;
+
+
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Payment {
+   @Id
+   @GeneratedValue
+   Long id;
+   Date date;
+   Double amount;
+   @OneToOne
+   @JoinColumn(name = "order_id")
+   Order order;
+   @OneToOne
+   @JoinColumn(name = "user_id")
+   Customer customer;
+   @OneToOne
+   @JoinColumn(name = "payment_type_id")
+   PaymentType paymentType;
+
+   public Payment(Date date, Double amount) {
+       this.date = date;
+       this.amount = amount;
+   }
+}
