3742612b3c862d7b6c86234e905b2ba7a1ac39f3
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2023-04-24 00:17:26 -0500

    deleted old directory

--- Server/src/main/java/miu/edu/pm/project/onlineshoppingcartsystem/email/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package miu.edu.pm.project.onlineshoppingcartsystem.email.service;
-
-import miu.edu.pm.project.onlineshoppingcartsystem.email.dto.EmailDto;
-
-public interface EmailService {
-
-    public void sendEmail(EmailDto email);
-}
